505756a48dd3fef969148c119663e5c7eee8977b
Rafal Gajdulewicz <gajrafal@amazon.com>
2013-09-02 17:24:13 -0700

    Better naming

--- 2/2.go
+++ 2/2.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	sum := 0
+	curr := 1
 	prev := 1
-	prev2 := 1
-	for prev <= 4000000 {
-		tmp := prev
-		prev = prev + prev2
-		prev2 = tmp
-		if prev%2 == 0 {
-			sum += prev
+	for curr <= 4000000 {
+		tmp := curr
+		curr = curr + prev
+		prev = tmp
+		if curr%2 == 0 {
+			sum += curr
 		}
 	}
 	fmt.Println(sum)
