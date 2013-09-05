fbe3145728ced5e18f3c139446c1208fd805670a
Rafal Gajdulewicz <gajrafal@amazon.com>
2013-09-04 22:51:44 -0700

    Add GCD and prime factors algorithms

--- utils/utils.go
+++ utils/utils.go
@@ -11,3 +11,35 @@ func IsPrime(num int) bool {
 	}
 	return true
 }
+
+func Gcd(a, b int) int {
+	var min, max int
+	if a < b {
+		min, max = a, b
+	} else {
+		min, max = b, a
+	}
+	for min != 0 {
+		max, min = min, max%min
+	}
+	return max
+}
+
+func PrimeFactors(n int) []int {
+	factors := make([]int, 0)
+	d := 2
+	for n > 1 {
+		for n%d == 0 {
+			factors = append(factors, d)
+			n /= d
+		}
+		d += 1
+		if d*d > n {
+			if n > 1 {
+				factors = append(factors, n)
+			}
+			break
+		}
+	}
+	return factors
+}
