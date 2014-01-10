e32d6f2e1f81b232a7be4effb8ebf1808070385e
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-01-09 20:26:07 -0800

    Parallel version of problem 10

---
+++ 10p.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"./utils"
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	max := 2000000
+	primes := make(chan int,max)
+	sum := make(chan int)
+	var done sync.WaitGroup
+	go worker(primes, sum)
+	for i := 3; i < max; i += 2 {
+		done.Add(1)
+		go func(num int) {
+			if utils.IsPrime(num) {
+				primes <- num
+			}
+			done.Done()
+		}(i)
+	}
+	done.Wait()
+	close(primes)
+	fmt.Println(<-sum)
+}
+
+func worker(primes, send chan int){
+	sum := 2
+	for i:= range primes{
+		sum+=i
+	}
+	send <- sum
+}
